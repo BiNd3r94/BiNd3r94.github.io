eeb4e477db0f523fa8750e28969d15bd7410c6bb
Jan Binder <jan.binder@usu.com>
2024-09-02 08:17:52 +0200

    change for git pages

--- src/components/StiftungHomoeopathie.tsx
+++
@@ -1,8 +0,0 @@
-export const StiftungHomoeopathie = () => {
-  return (
-      <div className="c-stiftung-component">
-        <p><b>Stiftung Homöopathie Zertifikat</b></p>
-        <img className={"cert-image"} src={"/logo_shz.png"} alt={"Logo SHZ"}/>
-      </div>
-  )
-}
--- src/components/VerbandKlassischerHomoeopathen.tsx
+++
@@ -1,8 +0,0 @@
-export const VerbandKlassischerHomoeopathen = () => {
-  return (
-      <div className="c-stiftung-component">
-        <p><b>Verband klassischer Homöopathen</b></p>
-        <img className={"cert-image"} src={"/logo_vkhd.png"} alt={"Logo VKHD"}/>
-      </div>
-  )
-}
